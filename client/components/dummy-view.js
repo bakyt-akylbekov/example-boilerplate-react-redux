605ef95bee17c98adc50419f78a68b728741e88c
bakyt-akylbekov <mr.akylbekovbakyt@mail.ru>
2020-05-16 00:48:08 +0600

    add css

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -9,8 +9,13 @@ const Dummy = () => {
     <div>
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is dummy component<Link to="/dashboard">Dashboard</Link>
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10  text-center">
+          {/* eslint-disable-next-line react/button-has-type */}
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mb-4">
+            <Link to="/dashboard">Dashboard</Link>
+          </button>
+          <br />
+          This is dummy component
         </div>
       </div>
     </div>
--- client/components/home.js
+++ client/components/home.js
@@ -10,10 +10,26 @@ const Home = () => {
   return (
     <div>
       <Head title="Hello" />
-      <button type="button" onClick={() => setCounterNew(counter + 1)}>
-        updateCounter
-      </button>
-      <div> Hello World Dashboard {counter} </div> <Link to="/">Home</Link>
+      <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10  text-center">
+          {/* eslint-disable-next-line react/button-has-type */}
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded mb-4">
+            <Link to="/">Home</Link>
+          </button>
+          <br />
+          {/* eslint-disable-next-line react/button-has-type */}
+          <button
+            type="button"
+            onClick={() => setCounterNew(counter + 1)}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4"
+          >
+            Update Counter
+          </button>
+          <div>
+            What your favorite number? <p>{counter}</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
